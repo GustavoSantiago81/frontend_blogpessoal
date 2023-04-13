cfa6de19f1749bbc2b3595e4982927d42026ae72
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-12 22:31:39 -0300

    App.tsx chamando de funcao Box da Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/footer';
-import Home from './paginas/home/Home';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Grid } from "@material-ui/core";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/footer";
+import Home from "./paginas/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar/>
-      <h1>Oi...</h1>
-      <h2>Tudo bem?</h2>
-      <Home/>
+      <Navbar />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+      </Grid>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper } from '@material-ui/core';
+import {Box, Button} from "@mui/material";
 import "./Home.css";
 import { colors } from "@mui/material";
 import { blue } from "@material-ui/core/colors";
 
 function Home() {
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8}>
-            <Paper style={{ height: "100vh", background: "grey" }} />
-        </Grid>
-        <Grid item container direction="column" sm={4} spacing={2}>
-            <Grid item>
-                <Paper style={{ height: "49vh", background: "green" }} />
-            </Grid>
-            <Grid item>
-                <Paper style={{ height: "49vh", background: "grey" }} />
-            </Grid>
-
-        </Grid>
-      </Grid>
-    </>
-  );
+    return (
+        <>
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Titulo</h1>
+                    </Box>
+                    <Box  display="flex" justifyContent="center" p={2}>
+                        <Button>Texto 1</Button>
+                        <Button>Texto 2</Button>
+                    </Box>
+                </Box>
+                
+            </Paper>
+        </>
+    );
 }
 
 export default Home;
