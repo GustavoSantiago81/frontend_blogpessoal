0669a8f754c3a15b92cfa7a2a01aef9867f98aa5
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-20 15:09:02 -0300

    bug postagem.tema? .descricao resolvido

--- src/components/postagens/listaPostagens/ListaPostagens.tsx
+++ src/components/postagens/listaPostagens/ListaPostagens.tsx
@@ -50,7 +50,7 @@ function ListaPostagens() {
                 {postagem.texto}
               </Typography>
               <Typography variant="body1" component="p">
-                {postagem.tema}
+                {postagem.tema?.descricao}
               </Typography>
             </CardContent>
             <CardActions>
