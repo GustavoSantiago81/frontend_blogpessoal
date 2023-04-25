b85da45d7fcf77d1220c78cff7b065b6b736fdd1
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-25 11:04:00 -0300

    add security redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,46 +7,45 @@ import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 import ListaTemas from "./components/temas/listaTemas/ListaTemas";
 import ListaPostagens from "./components/postagens/listaPostagem/ListaPostagem";
 import CadastroTema from "./components/temas/cadastroTema/CadastroTema";
 import CadastroPostagem from "./components/postagens/cadastroPostagem/CadastroPostagem";
-import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem'
-import DeletarTema from './components/temas/deletarTema/DeletarTema'
+import DeletarPostagem from "./components/postagens/deletarPostagem/DeletarPostagem";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-    
+    <Provider store={store}>
+      <BrowserRouter>
       <Navbar />
-
       <div>
         <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/cadastrarusuario" element={<CadastroUsuario/>}/>
-
-            <Route path="/temas" element={<ListaTemas/>}/>            
-            <Route path="/postagens" element={<ListaPostagens/>}/>
-
-            <Route path="/cadastrarTema" element={<CadastroTema/>}/>            
-            <Route path="/atualizarTema/:id" element={<CadastroTema/>}/>            
-            <Route path="/deletarTema/:id" element={<DeletarTema/>}/>
-            // /:id é uma variavel
-            <Route path="/cadastrarPostagem" element={<CadastroPostagem/>}/>            
-            <Route path="/atualizarPostagem/:id" element={<CadastroPostagem/>}/>            
-            <Route path="/deletarPostagem/:id" element={<DeletarPostagem/>}/>            
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastrarusuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTemas />} />
+          <Route path="/postagens" element={<ListaPostagens />} />
+          <Route path="/cadastrarTema" element={<CadastroTema />} />
+          <Route path="/atualizarTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+          // /:id é uma variavel
+          <Route path="/cadastrarPostagem" element={<CadastroPostagem />} />
+          <Route path="/atualizarPostagem/:id" element={<CadastroPostagem />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
         </Routes>
       </div>
- 
-      <Footer />
 
+      <Footer />
     </BrowserRouter>
+    </Provider>
   );
 }
 
